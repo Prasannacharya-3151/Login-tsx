474d61e313dfb564b4c93e440dfe53e42413361a
Prasannacharya-3151 <prasannacharya428@gmail.com>
2025-05-16 22:31:11 +0530

    LoginForm and App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import React, from 'react'
+import React from 'react'
 import LoginForm from './Components/LoginForm'
 
 const App:React.FC=()=>{
   const handleLogin=(email:string, password:string)=>{
-
+    console.log("Email:",email);
+    console.log("Password",password);
+    alert(`Welcome ${email}!`)
   }
-}
 
 
 
-  return (
-    <>
-    hi
-    </>
-  )
+
+ 
+      return <LoginForm onLogin={handleLogin}/>
+  
 }
 
 export default App
