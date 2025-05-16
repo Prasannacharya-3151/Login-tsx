ccf4ad1f827612292b18d0e8ea96f3406f130dda
Prasannacharya-3151 <prasannacharya428@gmail.com>
2025-05-16 22:09:48 +0530

    LoginForm updated

--- src/Components/LoginForm.tsx
+++ src/Components/LoginForm.tsx
@@ -3,3 +3,20 @@ import React, {useState} from "react";
 interface LoginFormProps{
     onLogin: (email: string, password: string) => void;
 }
+
+const LoginForm:React.Fc<LoginFromProps> = ({onLogin}) =>{
+    const[email, setEmail]=useState<string>("")
+    const[password, setPassword]=useState<string>("")
+
+    const handleSubmit = (e:React.FormEvent)=>{
+        e.preventDefault();
+        if(!email || !password){
+            alert("please enter both email and password.")
+            return;
+        }
+        onLogin(email, password)
+    }
+    return(
+        
+    )
+}
